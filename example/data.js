87cb2e6f788e6f50a4f9fcd7c2eba0cc91c9310e
Victor amorim <victoramorimchagas@gmail.com>
2024-04-30 07:59:49 -0300

    Mostra o id dos recursos no header e da alguns nomes para exemplos

--- example/data.js
+++ example/data.js
@@ -1,43 +1,43 @@
 var ganttData = [
     {
-        id: 1, name: "Recurso 1", series: [
+        id: 1, name: "Camisetas", series: [
             { name: "Planejado", start: new Date(2024, 0, 1, 8, 0), end: new Date(2024, 0, 3, 17, 0) },
             { name: "Real", start: new Date(2024, 0, 2, 10, 0), end: new Date(2024, 0, 5, 15, 30), color: "#f0f0f0" }
         ]
     }, 
     {
-        id: 2, name: "Recurso 2", series: [
+        id: 2, name: "Calças", series: [
             { name: "Planejado", start: new Date(2024, 0, 5, 9, 0), end: new Date(2024, 0, 20, 16, 30) },
             { name: "Real", start: new Date(2024, 0, 6, 8, 0), end: new Date(2024, 0, 17, 17, 0), color: "#f0f0f0" },
             { name: "Projetado", start: new Date(2024, 0, 6, 8, 0), end: new Date(2024, 0, 17, 17, 0), color: "#e0e0e0" }
         ]
     }, 
     {
-        id: 3, name: "Recurso 3", series: [
+        id: 3, name: "Tennis", series: [
             { name: "Planejado", start: new Date(2024, 0, 11, 7, 30), end: new Date(2024, 1, 3, 18, 0) },
             { name: "Real", start: new Date(2024, 0, 15, 9, 0), end: new Date(2024, 1, 3, 16, 45), color: "#f0f0f0" }
         ]
     }, 
     {
-        id: 4, name: "Recurso 4", series: [
+        id: 4, name: "Blusas", series: [
             { name: "Planejado", start: new Date(2024, 1, 1, 8, 0), end: new Date(2024, 1, 3, 17, 30) },
             { name: "Real", start: new Date(2024, 1, 1, 8, 0), end: new Date(2024, 1, 5, 16, 0), color: "#f0f0f0" }
         ]
     },
     {
-        id: 5, name: "Recurso 5", series: [
+        id: 5, name: "Shorts", series: [
             { name: "Planejado", start: new Date(2024, 0, 5, 9, 0), end: new Date(2024, 0, 20, 16, 30) },
             { name: "Real", start: new Date(2024, 0, 6, 8, 0), end: new Date(2024, 0, 17, 17, 0), color: "#f0f0f0" },
             { name: "Projetado", start: new Date(2024, 0, 6, 8, 0), end: new Date(2024, 0, 20, 16, 30), color: "#e0e0e0" }
         ]
     }, 
     {
-        id: 6, name: "Recurso 6", series: [
+        id: 6, name: "Camisas", series: [
             { name: "Planejado", start: new Date(2024, 0, 11, 7, 30), end: new Date(2024, 1, 3, 18, 0) }
         ]
     }, 
     {
-        id: 7, name: "Recurso 7", series: [
+        id: 7, name: "Gravatas", series: [
             { name: "Planejado", start: new Date(2024, 1, 1, 8, 0), end: new Date(2024, 1, 3, 17, 30) },
             { name: "Real", start: new Date(2024, 1, 1, 8, 0), end: new Date(2024, 1, 5, 16, 0), color: "#f0f0f0" }
         ]
